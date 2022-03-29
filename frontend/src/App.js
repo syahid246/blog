2271a74eeb49ce6943dcdbd648ea27b54e640534
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2022-03-29 16:58:50 +0700

    Insert Navbar to Page
    Add Navbar Section in Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
+import Navbar from "./components/Navbar";
+
 function App() {
     return <>
+        <Navbar className="border-b border-gray-500" />
         <div className="text-center mt-4">
             <h1 className="text-4xl font-medium mb-2">Blog Saya</h1>
             <p className="text-xl">Selamat Datang di Blog Saya!</p>
