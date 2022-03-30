6d19d3a89a6aea0b3e219bae407b353f175dc559
Kafaa Billahi Syahida <kafaabillaahisyahida135@gmail.com>
2022-03-30 09:19:59 +0700

    Make Navbar Sticky
    I'm already add Navbar Component in Home, but not Sticky.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,9 @@ import Navbar from '../components/Navbar'
 function Home()
 {
     return <>
-        <Navbar className="border-b border-gray-500" />
+        <div className="sticky top-0 w-full">
+            <Navbar className="border-b border-gray-500" />
+        </div>
         <div className="text-center mt-4">
             <h1 className="text-4xl font-medium mb-2">Blog Saya</h1>
             <p className="text-xl">Selamat Datang di Blog Saya!</p>
